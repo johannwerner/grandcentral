16fc313b0906eb1d1d86769e8adfa0b966859aef
Johann <johann@immedia.co.za>
2013-04-09 08:59:49 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grandcentral::Application.config.secret_token = '32b58b3389c9523df0fbf37a1774d5a263409a7dbb1bd6f66bc5f2c5b11d5e70a534bbc4a89ed568c081b942952abb7e9a6961d53e8e179940561669f1b55f4c'
